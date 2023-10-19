747272c43ac2f0f3c91321b4fb6cbe890c9c2895
mzinzun <zinzunmichael@gmail.com>
2023-10-19 10:43:24 -0700

    Made merge with main

--- src/GetShelter.jsx
+++ src/GetShelter.jsx
@@ -7,6 +7,7 @@ function GetShelter() {
       state: "",
    }
    const [formFields, setFormFields] = useState(emptyFields);
+   const [shelterList, setShelterList] = useState([]);
    function handleFormChange(e) {
       let temp = { ...formFields };
       temp[e.target.id] = e.target.value;
@@ -18,10 +19,17 @@ function GetShelter() {
       const options = {
          method: 'GET',
          headers: {
-            'X-RapidAPI-Key': '6fc3062f9amshb19471b6d09bba5p1e8ce0jsn4c9d612646c0',
-            'X-RapidAPI-Host': 'homeless-shelter.p.rapidapi.com'
+             'X-RapidAPI-Key': '6fc3062f9amshb19471b6d09bba5p1e8ce0jsn4c9d612646c0',
+             'X-RapidAPI-Host': 'homeless-shelter.p.rapidapi.com'
          }
-      };
+     };
+
+     const response = await fetch(url, options);
+     if (response.ok) {
+         const result = await response.json();
+         console.log(result)
+         setShelterList(result.shelters)
+     }
 
       try {
          const response = await fetch(url, options);
@@ -31,6 +39,19 @@ function GetShelter() {
          console.error(error);
       }
    }
+   const loadWeather = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=42.95898539151949&lon=-78.86170465446156&exclude=minutely,hourly,daily&units=imperial&appid=64993140304b1d95eb81734c42f0d52a`;
+
+
+      try {
+          const response = await fetch(url);
+          const result = await response.json();
+          console.log(result);
+      } catch (error) {
+          console.error(error);
+      }
+  }
+
    function getInfo(e){
       e.preventDefault();
       console.log(`${formFields.city}, ${formFields.state}`)
@@ -40,6 +61,7 @@ function GetShelter() {
    }
    useEffect(() => {
       console.log("GetShelter component running!")
+      loadShelter(), loadWeather()
       // loadShelter()
    }, [])
 
@@ -69,58 +91,18 @@ function GetShelter() {
       </div>
 
    )
-    const [shelterList, setShelterList] = useState([]);
-
-    const loadShelter = async () => {
-        const url = `https://homeless-shelter.p.rapidapi.com/state-city?state=NY&city=Buffalo`;
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': '6fc3062f9amshb19471b6d09bba5p1e8ce0jsn4c9d612646c0',
-                'X-RapidAPI-Host': 'homeless-shelter.p.rapidapi.com'
-            }
-        };
-
-        const response = await fetch(url, options);
-        if (response.ok) {
-            const result = await response.json();
-            console.log(result)
-            setShelterList(result.shelters)
-        }
-
-    }
-
-    const loadWeather = async () => {
-        const url = `https://api.openweathermap.org/data/2.5/weather?lat=42.95898539151949&lon=-78.86170465446156&exclude=minutely,hourly,daily&units=imperial&appid=64993140304b1d95eb81734c42f0d52a`;
-
-
-        try {
-            const response = await fetch(url);
-            const result = await response.json();
-            console.log(result);
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    useEffect(() => {
-        loadShelter(), loadWeather()
-    }, [])
 
 
 
 
 
 
-    return (
-        <div>
-            <h1>Shelters</h1>
 
-            {shelterList.map((item, i) =>
-                <div key={i}>{item.name}</div>
-            )}
-        </div>
-    )
+
+
+
+
+
 
 }
 export default GetShelter
