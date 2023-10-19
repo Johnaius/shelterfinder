5c6d8127740add9600aff87fab725fd034dacf15
JohnZ <johnzak80@gmail.com>
2023-10-19 15:07:41 -0400

    john-dev-branch

--- src/GetShelter.jsx
+++ src/GetShelter.jsx
@@ -1,100 +1,98 @@
-import React, {useEffect, useState} from "react";
-import {Link} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+import { SHELTERKEY, OPENWEATHERKEY } from "./assets/keys";
+
 
 function GetShelter() {
-   const emptyFields = {
-      city: "",
-      state: "",
-   }
-   const [formFields, setFormFields] = useState(emptyFields);
-   const [shelterList, setShelterList] = useState([]);
-   function handleFormChange(e) {
-      let temp = { ...formFields };
-      temp[e.target.id] = e.target.value;
-      setFormFields(temp);
-   }
-   const loadShelter = async (city,state) => {
-      console.log('load shelter running!')
-      const url = `https://homeless-shelter.p.rapidapi.com/state-city?state=${state}&city=${city}`;
-      const options = {
-         method: 'GET',
-         headers: {
-             'X-RapidAPI-Key': '6fc3062f9amshb19471b6d09bba5p1e8ce0jsn4c9d612646c0',
-             'X-RapidAPI-Host': 'homeless-shelter.p.rapidapi.com'
-         }
-     };
+    const emptyFields = {
+        city: "",
+        state: "",
+    }
+    const [formFields, setFormFields] = useState(emptyFields);
+    const [shelterList, setShelterList] = useState([]);
+    function handleFormChange(e) {
+        let temp = { ...formFields };
+        temp[e.target.id] = e.target.value;
+        setFormFields(temp);
+    }
+    const loadShelter = async (city, state) => {
+        console.log('load shelter running!')
+        const url = `https://homeless-shelter.p.rapidapi.com/state-city?state=${state}&city=${city}`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': `${SHELTERKEY}`,
+                'X-RapidAPI-Host': 'homeless-shelter.p.rapidapi.com'
+            }
+        };
 
-     const response = await fetch(url, options);
-     if (response.ok) {
-         const result = await response.json();
-         console.log(result)
-         setShelterList(result.shelters)
-     }
-   }
-   const loadWeather = async () => {
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=42.95898539151949&lon=-78.86170465446156&exclude=minutely,hourly,daily&units=imperial&appid=64993140304b1d95eb81734c42f0d52a`;
+        const response = await fetch(url, options);
+        let lat = ''
+        let lon = ''
+        if (response.ok) {
+            const result = await response.json();
+            coordinates = result.location.split(',')
+            lat = coordinates[0]
+            lon = coordinates[1]
+            console.log(result)
+            setShelterList(result.shelters)
+        }
+        const loadWeather = async () => {
 
-
-      try {
-          const response = await fetch(url);
-          const result = await response.json();
-          console.log(result);
-      } catch (error) {
-          console.error(error);
-      }
-  }
-
-   function getInfo(e){
-      e.preventDefault();
-      console.log(`${formFields.city}, ${formFields.state}`)
-      // loadShelter(formFields.city,formFields.state);
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=-${lon}&exclude=minutely,hourly,daily&units=imperial&appid=${OPENWEATHERKEY}`;
 
 
-   }
-   useEffect(() => {
-      console.log("GetShelter component running!")
-      loadShelter(), loadWeather()
-      // loadShelter()
-   }, [])
+            try {
+                const weatherResponse = await fetch(url);
+                const weatherResult = await weatherResponse.json();
+                console.log(weatherResult);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        loadWeather()
+
+    }
+
+    function getInfo(e) {
+        e.preventDefault();
+        console.log(`${formFields.city}, ${formFields.state}`)
+        // loadShelter(formFields.city,formFields.state);
 
 
-   return (
-      <div>
-         <h2>Search for Shelter near you!</h2>
-         <form>
-            <div className="mb-3">
-               <label htmlFor="city" className="form-label">City</label>
-               <input type="email" className="form-control" name="city" id="city" aria-describedby="emailHelp" />
-
-            </div>
-            <div className="mb-3">
-               <label htmlFor="state" className="form-label">State</label>
-               <input className="form-control"
-                  type="text"
-                  id="state"
-                  name="state"
-                  value = {formFields.state}
-                  onChange={handleFormChange}
-               />
-            </div>
-
-            <button type="submit" className="btn btn-primary" onClick={getInfo}>Submit</button>
-         </form>
-      </div>
-
-   )
+    }
+    useEffect(() => {
+        console.log("GetShelter component running!")
+        loadShelter()
+        // loadShelter()
+    }, [])
 
 
+    return (
+        <div>
+            <h2>Search for Shelter near you!</h2>
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="city" className="form-label">City</label>
+                    <input type="email" className="form-control" name="city" id="city" aria-describedby="emailHelp" />
 
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="state" className="form-label">State</label>
+                    <input className="form-control"
+                        type="text"
+                        id="state"
+                        name="state"
+                        value={formFields.state}
+                        onChange={handleFormChange}
+                    />
+                </div>
 
+                <button type="submit" className="btn btn-primary" onClick={getInfo}>Submit</button>
+            </form>
+        </div>
 
-
-
-
-
-
-
-
+    )
 
 }
 export default GetShelter
